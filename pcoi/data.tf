ef0634606e012276f76b2c08102cb658e6b33068
andy.wen <andy.wen@ontariohealth.ca>
2024-06-26 09:58:55 -0400

    check in

---
+++ pcoi/data.tf
@@ -0,0 +1,3 @@
+data "aws_lambda_function" "lambuda_authorizer" {
+  function_name = "nodeJsAuthorizer2"
+}
---
+++ pcoi/variables.tf
@@ -0,0 +1,14 @@
+variable "stage_name" {
+  default = "dev2"
+  type    = string
+}
+
+variable "backbone_url" {
+  default = "https://consumer.dit.ehealthontario.ca/devops/sandbox/backbone"
+  type    = string
+}
+
+variable "lob_url" {
+  default = "http://dummy.apps.dev2.ecp.ehealthontario.ca"
+  type    = string
+}
